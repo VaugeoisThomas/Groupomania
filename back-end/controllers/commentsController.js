920ffd32a5ed48efb7158e40bd7cd6e89b763c1c
Vaugeois Thomas <vaugeois.thomas@gmail.com>
2021-07-23 09:34:52 +0200

    Creation of deletion and adding comments

--- back-end/controllers/commentsController.js
+++ back-end/controllers/commentsController.js
@@ -11,9 +11,9 @@ exports.addComments = (req, res) => {
     ],
     (err, result) => {
         if(err){
-            res.status(400).json(errManagement.error(err.message));
+            return res.status(400).json(errManagement.error(err.message));
         } else {
-            res.status(201).json(errManagement.success("Votre commentaire à été ajouté"));
+            return res.status(201).json(errManagement.success("Votre commentaire à été ajouté"));
         }
     });
 };
@@ -24,14 +24,29 @@ exports.getAllComments = (req, res) => {
 exports.deleteComment = (req, res) => {
     bdd.query(Users.selectUsersById, req.userId, (err, result) => {
         if(err) {
-            res.status(400).json(errManagement.error(err.message));
+            return res.status(400).json(errManagement.error(err.message));
         } else {
             if(result[0] != undefined) {
+                const User = result[0]
                 bdd.query(Comments.selectOneComment, req.params.id, (err, res) => {
                     if(err) {
-                        
+                      return res.status(404).json(errManagement.error(err.message));  
+                    } else {
+                        if(req.body.users_id == result[0].users_id || User.isAdmin == 1){
+                            bdd.query(Comments.deleteComment, req.params.id, (err, res) => {
+                                if(err){
+                                    return res.status(400).json(errManagement.error(err.message));
+                                } else {
+                                    return res.status(200).json(errManagement.success("Commantaire supprimé"));
+                                }
+                            });
+                        } else {
+                            return res.status(403).json(errManagement.error("Vous ne pouvez pas supprimer ce commentaire"));
+                        }
                     }
-                })
+                });
+            } else {
+                return res.status(404).json(errManagement.error("Commentaire introuvable !"))
             }
         }
     })
