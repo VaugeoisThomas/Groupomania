8417bf9949fcc7976b42a90d35114e2db66c2b0c
Vaugeois Thomas <61732331+VaugeoisThomas@users.noreply.github.com>
2021-10-01 15:20:42 +0200

    Update 20210919125036-Admin.js

--- backend/seeders/20210919125036-Admin.js
+++ backend/seeders/20210919125036-Admin.js
@@ -1,10 +1,12 @@
 'use strict';
+const bcrypt = require('bcrypt');
+const maskdata = require('maskdata');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [{
-      email: 'admin@groupomania.com',
-      password: 'Password09',
+      email: maskdata.maskEmail2('admin@groupomania.com'),
+      password: await bcrypt.hash('Password09', 10),
       username: 'Admin',
       isAdmin: 1,
       createdAt: new Date(),
